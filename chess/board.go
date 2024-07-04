0b06e1b94d65ce2507c6095edf44880dc99a64a9
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-07-03 22:00:21 -0600

    Correct math and comprehensive proof.

--- chess/board.go
+++ chess/board.go
@@ -1,14 +1,13 @@
 package chess
 
-type BitBoard int64
+type BitBoard uint64
 
-func (this BitBoard) IsOccupied(square int) bool {
-	var mask uint64
-	mask = 1 << uint64(this)
-	return (uint64(square) & mask) == 0
+func (this *BitBoard) mask(bit int) BitBoard {
+	return BitBoard(1 << uint64(bit))
+}
+func (this *BitBoard) IsOccupied(square int) bool {
+	return *this&this.mask(square) > 0
 }
 func (this *BitBoard) Occupy(square int) {
-	var mask BitBoard
-	mask = BitBoard(1 << int64(square))
-	*this |= mask
+	*this |= this.mask(square)
 }
--- chess/board_test.go
+++ chess/board_test.go
@@ -1,25 +1,21 @@
 package chess
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/mdwhatcott/testing/should"
 )
 
-func TestBitBoardSuite(t *testing.T) {
-	should.Run(&BitBoardSuite{T: should.New(t)}, should.Options.UnitTests())
-}
-
-type BitBoardSuite struct {
-	*should.T
-}
-
-func (this *BitBoardSuite) Test() {
+func TestBitBoard(t *testing.T) {
 	board := BitBoard(0)
-	this.So(board.IsOccupied(1), should.BeFalse)
-	this.Println(board)
-
-	board.Occupy(1)
-	this.So(board.IsOccupied(1), should.BeTrue)
-	this.Println(board)
+	should.So(t, board, should.Equal, 0)
+	for square := 0; square < 64; square++ {
+		t.Run(fmt.Sprint(square), func(t *testing.T) {
+			should.So(t, board.IsOccupied(square), should.BeFalse)
+			board.Occupy(square)
+			should.So(t, board.IsOccupied(square), should.BeTrue)
+		})
+	}
+	should.So(t, board, should.Equal, BitBoard(0xffffffffffffffff))
 }
